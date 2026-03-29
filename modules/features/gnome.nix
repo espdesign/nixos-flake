9ea797d378a934e0aea9d72055a006728c7cd6c8
espdesign <evanpendergraft@gmail.com>
2026-03-29 13:24:17 -0400

    add gnome settings

--- modules/features/gnome.nix
+++ modules/features/gnome.nix
@@ -8,6 +8,8 @@
       services.displayManager.gdm.enable = true;
       services.desktopManager.gnome.enable = true;
 
+      environment.etc."backgrounds/wallpaper.jpg".source = ./wallpaper.jpg;
+
       # To disable installing GNOME's suite of applications
       # and only be left with GNOME shell.
       services.gnome.core-apps.enable = false;
@@ -22,6 +24,35 @@
       environment.systemPackages = with pkgs; [
         gnomeExtensions.appindicator
       ];
-    };
 
+      programs.dconf.profiles.user.databases = [
+        {
+          lockAll = true; # prevents overriding
+          settings = {
+            "org/gnome/shell" = {
+              disable-user-extensions = false;
+              # Enable the app indicator extension (tray icons)
+              enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" ];
+              favorite-apps = [
+                "org.gnome.Nautilus.desktop"
+                "firefox.desktop"
+                "com.mitchellh.ghostty.desktop"
+              ];
+            };
+
+            "org/gnome/desktop/interface" = {
+              show-battery-percentage = true;
+              clock-format = "12h";
+              color-scheme = "prefer-dark";
+            };
+
+            # Set desktop background for dark and light mode
+            "org/gnome/desktop/background" = {
+              picture-uri = "file:///etc/backgrounds/wallpaper.jpg";
+              picture-uri-dark = "file:///etc/backgrounds/wallpaper.jpg";
+            };
+          };
+        }
+      ];
+    };
 }
